830ce49dced7aac87f389d7162b1db0ff4168175
Larry Ditton <larry@flexpa.com>
2025-04-30 13:27:52 -0600

    Initial commit
    Generated by create-expo-stack 2.14.3

---
+++ app/_layout.tsx
@@ -0,0 +1,27 @@
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { TamaguiProvider } from 'tamagui';
+
+import config from '../tamagui.config';
+
+export default function Layout() {
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) return null;
+
+  return (
+    <TamaguiProvider config={config}>
+      <Stack />
+    </TamaguiProvider>
+  );
+}
